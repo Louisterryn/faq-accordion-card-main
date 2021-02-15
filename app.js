50294b0be983ef5f0721fd2d16a946e76aa00b27
Louisterryn <terrynlouis@gmail.com>
2021-02-15 22:00:57 +0100

    final

--- app.js
+++ app.js
@@ -5,15 +5,7 @@ let i;
 
 for(i = 0; i < accordions.length; i++){
     accordions[i].addEventListener('click', function(){
-        
 
-        // let panel = this.nextElementSibling; // On recupère l'élément qui suit et on le stocke dans la variable panel
-        // if(panel.style.display === 'block'){
-        //     panel.style.display = 'none';
-        // }else{
-        //     panel.style.display = 'block';
-        // }
-        // 
         let activated = [...this.classList].includes('active'); // On stocke si le containe contient la class active
         [...accordions].forEach(function (accordion){
             if([...accordion.classList].includes('active')){
